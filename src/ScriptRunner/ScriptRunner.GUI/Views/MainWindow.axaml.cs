72e7a55c2cf15d3390f9a13ed7cf1243b59be98b
Kapu <kapu1500@gmail.com>
2022-04-12 20:35:28 +0200

    Default avalonia project files

---
+++ src/ScriptRunner/ScriptRunner.GUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ScriptRunner.GUI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
