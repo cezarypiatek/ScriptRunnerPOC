b312021da1654f8bd951d6982e04ee7485e96c1a
Kapu <kapu1500@gmail.com>
2022-04-12 22:56:12 +0200

    Spike generating textBox dynamically

--- src/ScriptRunner/ScriptRunner.GUI/ViewModels/MainWindowViewModel.cs
+++ src/ScriptRunner/ScriptRunner.GUI/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Avalonia.Controls;
+using ReactiveUI;
 
 namespace ScriptRunner.GUI.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private ObservableCollection<IControl> _controlsCollection;
+        public ObservableCollection<IControl> ControlsCollection
+        {
+            get => _controlsCollection;
+            private set => this.RaiseAndSetIfChanged(ref _controlsCollection, value);
+        }
+
+        public void AddNewTextBoxCommand()
+        {
+            ControlsCollection.Add(new TextBox
+            {
+                Text = "test text"
+            });
+        }
+
+        public MainWindowViewModel()
+        {
+            _controlsCollection = new ObservableCollection<IControl>
+            {
+                new TextBox
+                {
+                    Text = "test text"
+                }
+            };
+        }
+
+
     }
 }
