d3f3028b60d161e34be7a09fd5f69b6671eebacd
Kapu <kapu1500@gmail.com>
2022-04-27 22:27:55 +0200

    Finally some working dynamic control creation

---
+++ src/ScriptRunner/ScriptRunner.GUI/ParamsPanelFactory.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using Avalonia.Controls;
+using Avalonia.Layout;
+using Avalonia.Media;
+using ScriptRunner.GUI.ScriptConfigs;
+
+namespace ScriptRunner.GUI;
+
+public class ParamsPanelFactory
+{
+    public ParamsPanel Create(IEnumerable<ScriptParam> parameters)
+    {
+        var paramsPanel = new StackPanel();
+        var controlRecords = new List<IControlRecord>();
+
+        foreach (var param in parameters)
+        {
+            var controlRecord = CreateControlRecord(param.Prompt);
+            var actionPanel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                Children =
+                {
+                    new Label
+                    {
+                        Content = param.Name
+                    },
+                    controlRecord.Control
+                }
+            };
+            
+            paramsPanel.Children.Add(actionPanel);
+            controlRecords.Add(controlRecord);
+        }
+
+        return new ParamsPanel
+        {
+            Panel = paramsPanel,
+            ControlRecords = controlRecords
+        };
+    }
+
+    private static IControlRecord CreateControlRecord(PromptType promptType)
+    {
+        switch (promptType)
+        {
+            case PromptType.Text:
+                var control = new TextBox();
+                return new TextControl
+                {
+                    Control = control
+                };
+            case PromptType.Password:
+                return new TextControl
+                {
+                    Control = new TextBox
+                    {
+                        PasswordChar = '*'
+                    }
+                };
+            case PromptType.Dropdown:
+                return new DropdownControl
+                {
+                    Control = new ComboBox
+                    { 
+                        Items = new List<string>
+                        {
+                            "SomeItem1",
+                            "SomeItem2"
+                        }
+                    }
+                };
+            //case PromptType.Multiselect:
+            //    break;
+            case PromptType.Datepicker:
+                return new DatePickerControl
+                {
+                    Control = new DatePicker()
+                };
+            case PromptType.Checkbox:
+                return new CheckboxControl
+                {
+                    Control = new CheckBox()
+                };
+            case PromptType.Multilinetext:
+                return new TextControl
+                {
+                    Control = new TextBox
+                    {
+                        TextWrapping = TextWrapping.Wrap,
+                        AcceptsReturn = true,
+                        Height = 60
+                    }
+        };
+            default:
+                throw new ArgumentOutOfRangeException(nameof(promptType), promptType, null);
+        }
+    }
+}
+
+
+public class ParamsPanel
+{
+    public IPanel Panel { get; set; }
+
+    public IEnumerable<IControlRecord> ControlRecords { get; set; }
+}
+
+public class CheckboxControl : IControlRecord
+{
+    public IControl Control { get; set; }
+    public Type ValueType => typeof(bool);
+    public dynamic GetValue()
+    {
+        return ((CheckBox)Control).IsChecked ?? false;
+    }
+}
+
+public class DatePickerControl : IControlRecord
+{
+    public IControl Control { get; set; }
+    public Type ValueType => typeof(DateTime);
+    public dynamic GetValue()
+    {
+        return ((DatePicker)Control).SelectedDate?.DateTime;
+    }
+}
+
+public class DropdownControl : IControlRecord
+{
+    public IControl Control { get; set; }
+    public Type ValueType => typeof(string);
+    public dynamic GetValue()
+    {
+        return ((ComboBox)Control).SelectedItem?.ToString();
+    }
+}
+
+public class TextControl : IControlRecord
+{
+    public IControl Control { get; set; }
+    public Type ValueType => typeof(string);
+    public dynamic GetValue()
+    {
+        return ((TextBox)Control).Text;
+    }
+}
+
+public interface IControlRecord
+{
+    IControl Control { get; set; }
+
+    Type ValueType { get; }
+
+    dynamic GetValue();
+}
--- src/ScriptRunner/ScriptRunner.GUI/ViewModels/MainWindowViewModel.cs
+++ src/ScriptRunner/ScriptRunner.GUI/ViewModels/MainWindowViewModel.cs
@@ -1,65 +1,83 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Reactive.Linq;
-using Avalonia;
 using Avalonia.Controls;
-using DynamicData;
 using ReactiveUI;
 using ScriptRunner.GUI.ScriptReader;
-using Console = System.Console;
 
-namespace ScriptRunner.GUI.ViewModels
+namespace ScriptRunner.GUI.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
 {
-    public class MainWindowViewModel : ViewModelBase
+    /// <summary>
+    /// Contains panels with generated controls for every defined action
+    /// </summary>
+    private ObservableCollection<IPanel> _actionPanelsCollection;
+    public ObservableCollection<IPanel> ActionPanelsCollection
     {
-        private ObservableCollection<IControl> _controlsCollection;
-        public ObservableCollection<IControl> ControlsCollection
-        {
-            get => _controlsCollection;
-            private set => this.RaiseAndSetIfChanged(ref _controlsCollection, value);
-        }
+        get => _actionPanelsCollection;
+        private set => this.RaiseAndSetIfChanged(ref _actionPanelsCollection, value);
+    }
 
-        private ObservableCollection<string> _actions;
-        public ObservableCollection<string> Actions
-        {
-            get => _actions;
-            private set => this.RaiseAndSetIfChanged(ref _actions, value);
-        }
+    /// <summary>
+    /// Contains list of actions defined in json file
+    /// </summary>
+    private ObservableCollection<string> _actions;
+    public ObservableCollection<string> Actions
+    {
+        get => _actions;
+        private set => this.RaiseAndSetIfChanged(ref _actions, value);
+    }
 
-        public MainWindowViewModel()
-        {
-            _controlsCollection = new ObservableCollection<IControl>();
-            _actions = new ObservableCollection<string>();
+    public MainWindowViewModel()
+    {
+        ActionPanelsCollection = new ObservableCollection<IPanel>();
+        Actions = new ObservableCollection<string>();
 
-            BuildUi();
-        }
-        
-        private IObservable<string> _textObserver;
+        BuildUi();
+    }
 
-        private void BuildUi()
+    private IEnumerable<IControlRecord> _controlRecords;
+
+    private void BuildUi()
+    {
+        var config = ScriptConfigReader.Load();
+        foreach (var action in config.Actions)
         {
-            var config = ScriptConfigReader.Load();
-            foreach (var action in config.Actions)
+            Actions.Add(action.Name);
+                
+            // Action panel could be used by creating custom user control with Name, description etc.
+            // Just ParamsPanel should be generated dynamically
+            var actionPanel = new StackPanel();
+            actionPanel.Children.AddRange(new List<IControl>
             {
-                Actions.Add(action.Name);
+                new Label { Content = action.Name },
+                new TextBlock { Text = action.Description },
+                new TextBlock { Text = action.Command },
+                new Label { Content = "Parameters: " }
+            });
 
-                // creating field
-                var testTextBox = new TextBox();
-                _textObserver = testTextBox.GetObservable(TextBox.TextProperty);
-                var disposableToDispose = _textObserver.Subscribe();
-                ControlsCollection.Add(testTextBox);
+            // Create IPanel with controls for all parameters
+            var paramsPanel = new ParamsPanelFactory().Create(action.Params);
 
+            // Add panel with param controls to action panel
+            actionPanel.Children.Add(paramsPanel.Panel);
 
+            // Add action panel to root container with all action panels
+            ActionPanelsCollection.Add(actionPanel);
 
-                ControlsCollection.AddRange(UiFactory.BuildControls(action));
-            }
+            // Write down param controls to read easier later - TODO: figure out better way, support multiple actions
+            _controlRecords = paramsPanel.ControlRecords;
         }
+    }
 
-        public void RunScript()
+    public void RunScript()
+    {
+        foreach (var controlRecord in _controlRecords)
         {
-            var wartosc = _textObserver.FirstAsync().GetAwaiter().GetResult();
-
+            // This is definitely not pretty, should be using some ReactiveUI observables to read values?
+            var controlValue = controlRecord.GetValue();
+            var convertedValue = Convert.ChangeType(controlValue, controlRecord.ValueType);
         }
     }
 }
