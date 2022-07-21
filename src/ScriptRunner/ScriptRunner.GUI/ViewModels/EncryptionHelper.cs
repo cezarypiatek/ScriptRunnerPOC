3de39e8d732e3566231594640f0792d9a543d9d4
Cezary Piątek <cezarypiatek@live.com>
2022-07-21 20:17:32 +0200

    Change Vault implementation to use System.Security.Cryptography.ProtectedData

---
+++ src/ScriptRunner/ScriptRunner.GUI/ViewModels/EncryptionHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ScriptRunner.GUI.ViewModels;
+
+public static class EncryptionHelper
+{
+    private static byte[] EntropyKey = Encoding.ASCII.GetBytes("80CD0C6D-74D3-4E6D-9E4F-ECA485E69FC7");
+
+    public static string Encrypt(string value)
+    {
+        byte[] data = Encoding.ASCII.GetBytes(value);
+        string protectedData = Convert.ToBase64String(ProtectedData.Protect(data, EntropyKey, DataProtectionScope.CurrentUser));
+        return protectedData;
+    }
+
+    public static string Decrypt(string value)
+    {
+        byte[] protectedData = Convert.FromBase64String(value);
+        string data = Encoding.ASCII.GetString(ProtectedData.Unprotect(protectedData, EntropyKey, DataProtectionScope.CurrentUser));
+        return data;
+    }
+}
