07ef34fa8c5f5f65b62968672b64a2c3ad184ded
Kapu <kapu1500@gmail.com>
2022-08-31 12:34:24 +0200

    Add config repos updater

---
+++ src/ScriptRunner/ScriptRunner.GUI/BackgroundTasks/OutdatedRepositoryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ScriptRunner.GUI.BackgroundTasks;
+
+public class OutdatedRepositoryModel
+{
+    public string Path { get; set; }
+
+    public OutdatedRepositoryModel(string path)
+    {
+        Path = path;
+    }
+}
---
+++ src/ScriptRunner/ScriptRunner.GUI/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace ScriptRunner.GUI.Converters;
+
+public class CountToVisibilityConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var count = (int?)value;
+        return count > 0;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
