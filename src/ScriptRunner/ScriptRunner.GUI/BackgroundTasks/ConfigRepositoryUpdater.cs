a6386d4355d92e6be650b0a9d1bd567d18fbb7d3
Kapu <kapu1500@gmail.com>
2022-09-03 15:41:42 +0200

    Using git credentials for config repo updates

--- src/ScriptRunner/ScriptRunner.GUI/BackgroundTasks/ConfigRepositoryUpdater.cs
+++ src/ScriptRunner/ScriptRunner.GUI/BackgroundTasks/ConfigRepositoryUpdater.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+using Microsoft.Alm.Authentication;
 using ScriptRunner.GUI.Settings;
 
 namespace ScriptRunner.GUI.BackgroundTasks;
@@ -23,11 +25,16 @@ public static class ConfigRepositoryUpdater
             using var repo = new Repository(entry.Path);
             var remote = repo.Network.Remotes["origin"];
             var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
-            Commands.Fetch(repo, remote.Name, refSpecs, null, logMessage);
+
+            var options = new FetchOptions
+            {
+                CredentialsProvider = CreateCredentialsHandler()
+            };
+
+            Commands.Fetch(repo, remote.Name, refSpecs, options, logMessage);
 
             if (repo.Head.TrackingDetails.BehindBy > 0)
             {
-                // Repo is out of date
                 outOfDateRepos.Add(new OutdatedRepositoryModel(entry.Path));
             }
         }
@@ -41,23 +48,37 @@ public static class ConfigRepositoryUpdater
 
         var options = new PullOptions
         {
-            FetchOptions = new FetchOptions()
+            FetchOptions = new FetchOptions
+            {
+                CredentialsProvider = CreateCredentialsHandler()
+            }
         };
 
-        //options.FetchOptions.CredentialsProvider = new CredentialsHandler(
-        //    (url, usernameFromUrl, types) =>
-        //        new UsernamePasswordCredentials()
-        //        {
-        //            Username = USERNAME,
-        //            Password = PASSWORD
-        //        });
-
-        // User information to create a merge commit
         var signature = repo.Config.BuildSignature(DateTimeOffset.Now);
 
         var mergeResult = Commands.Pull(repo, signature, options);
         var success = mergeResult.Status != MergeStatus.Conflicts;
 
         return Task.FromResult(success);
+    }
+
+    private static CredentialsHandler CreateCredentialsHandler()
+    {
+        var secrets = new SecretStore("git");
+        var auth = new BasicAuthentication(secrets);
+
+        return new CredentialsHandler((repoUrl, usernameFromUrl, types) =>
+        {
+            var url = new Uri(repoUrl);
+            var credentials = auth.GetCredentials(new TargetUri(url.GetLeftPart(UriPartial.Authority)));
+
+            //TODO: if credentials not found - what to do? Prompt/ignore/show message?
+
+            return new UsernamePasswordCredentials
+            {
+                Username = credentials.Username,
+                Password = credentials.Password
+            };
+        });
     }
 }
