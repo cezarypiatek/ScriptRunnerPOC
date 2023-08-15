77bfa826f60a397053932445e637100a4b23abc9
Cezary Piątek <cezarypiatek@live.com>
2023-08-15 08:26:42 +0200

    Improve UX of search box

--- src/ScriptRunner/ScriptRunner.GUI/Views/SideMenu.axaml.cs
+++ src/ScriptRunner/ScriptRunner.GUI/Views/SideMenu.axaml.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
-using Avalonia.VisualTree;
 using ScriptRunner.GUI.ScriptConfigs;
 using ScriptRunner.GUI.Settings;
 using ScriptRunner.GUI.ViewModels;
@@ -15,8 +17,11 @@ namespace ScriptRunner.GUI.Views;
 
 public partial class SideMenu : UserControl
 {
+    private IDisposable openSearchBoxEventHandler;
+
     public SideMenu()
     {
+        
         InitializeComponent();
     }
 
@@ -29,27 +34,35 @@ public partial class SideMenu : UserControl
     {
         if (DataContext is MainWindowViewModel viewModel)
         {
+            openSearchBoxEventHandler?.Dispose();
             var recent = AppSettingsService.Load().Recent.Values.OrderByDescending(x => x.Timestamp).Take(5).ToArray();
-            var popup = new SearchBox(viewModel?.Actions ?? new List<ScriptConfig>(), recent);
-        
+            var popupContent = new SearchBox(viewModel?.Actions ?? new List<ScriptConfig>(), recent);
+
             if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                var sourceWindow =  desktop.MainWindow;
-                popup.KeyUp += (o, args) =>
+                var popup = desktop.MainWindow.Find<Popup>("PopupContainer");
+                popup.Child = popupContent;
+                popup.VerticalOffset = desktop.MainWindow.Height * 0.3;
+
+                popupContent.KeyUp += (o, args) =>
                 {
                     if (args.Key == Key.Escape)
                     {
                         popup.Close();
                     }
                 };
-            
-                if (await popup.ShowDialog<ScriptConfigWithArgumentSet>(sourceWindow) is { } selectedCommand)
+                popup.Open();
+                this.openSearchBoxEventHandler= Observable.FromEventPattern(popupContent, nameof(popupContent.ResultSelected)).Subscribe(pattern =>
                 {
-                    viewModel.SelectedAction = selectedCommand.Config;
-                    viewModel.SelectedArgumentSet = selectedCommand.ArgumentSet;
-                }
+                    popup.Close();
+
+                    if (pattern.EventArgs is SearchBox.ResultSelectedEventArgs {Result: { } selectedCommand})
+                    {
+                        viewModel.SelectedAction = selectedCommand.Config;
+                        viewModel.SelectedArgumentSet = selectedCommand.ArgumentSet;
+                    }
+                });
             }
         }
-      
     }
 }
