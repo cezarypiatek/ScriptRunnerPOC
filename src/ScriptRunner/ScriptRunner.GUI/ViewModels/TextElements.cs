00d54af16b48c077c7a7dd36f06814ab91958471
Cezary Piatek <cezarypiatek@live.com>
2024-05-28 21:32:37 +0200

    Improve output processing performance

---
+++ src/ScriptRunner/ScriptRunner.GUI/ViewModels/TextElements.cs
@@ -0,0 +1,12 @@
+using Avalonia.Media;
+
+namespace ScriptRunner.GUI.ViewModels;
+
+public abstract record OutputElement;
+
+public record LineEnding : OutputElement
+{
+    public static readonly LineEnding Instance = new LineEnding();
+}
+public record TextSpan(string Text, IBrush Foreground, IBrush BackGround,  bool IsBold = false, bool IsItalic = false, bool IsUnderline = false):OutputElement;
+public record Link(string Text, string? Url = null):OutputElement;
