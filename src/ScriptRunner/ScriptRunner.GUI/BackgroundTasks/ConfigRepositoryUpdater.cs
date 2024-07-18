8f5d448ecbdd659b10efd96632f33643ca70afc9
Cezary Piatek <cezarypiatek@live.com>
2024-07-18 19:52:50 +0200

    Cleanup git support code

--- src/ScriptRunner/ScriptRunner.GUI/BackgroundTasks/ConfigRepositoryUpdater.cs
+++ src/ScriptRunner/ScriptRunner.GUI/BackgroundTasks/ConfigRepositoryUpdater.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,66 +54,7 @@ class CliRepositoryClient : IRepositoryClient
     }
 }
 
-public class LibGit2SharpRepositoryClient : IRepositoryClient
-{
-    public Task<bool> IsOutdated(string repoPath)
-    {
-        var logMessage = string.Empty;
-        using var repo = new Repository(repoPath);
-        var remote = repo.Network.Remotes["origin"];
-        var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
 
-        var options = new FetchOptions
-        {
-            CredentialsProvider = CreateCredentialsHandler()
-        };
-
-        Commands.Fetch(repo, remote.Name, refSpecs, options, logMessage);
-        var isOutdated = repo.Head.TrackingDetails.BehindBy > 0;
-        return Task.FromResult(isOutdated);
-    }
-
-    public Task<bool> PullRepository(string path)
-    {
-        using var repo = new Repository(path);
-
-        var options = new PullOptions
-        {
-            FetchOptions = new FetchOptions
-            {
-                CredentialsProvider = CreateCredentialsHandler()
-            }
-        };
-
-        var signature = repo.Config.BuildSignature(DateTimeOffset.Now);
-
-        var mergeResult = Commands.Pull(repo, signature, options);
-        var success = mergeResult.Status != MergeStatus.Conflicts;
-
-        return Task.FromResult(success);
-    }
-
-    private static CredentialsHandler CreateCredentialsHandler()
-    {
-        throw new NotImplementedException();
-        //var secrets = new SecretStore("git");
-        //var auth = new BasicAuthentication(secrets);
-
-        //return new CredentialsHandler((repoUrl, usernameFromUrl, types) =>
-        //{
-        //    var url = new Uri(repoUrl);
-        //    var credentials = auth.GetCredentials(new TargetUri(url.GetLeftPart(UriPartial.Authority)));
-
-        //    //TODO: if credentials not found - what to do? Prompt/ignore/show message?
-
-        //    return new UsernamePasswordCredentials
-        //    {
-        //        Username = credentials.Username,
-        //        Password = credentials.Password
-        //    };
-        //});
-    }
-}
 
 public static class ConfigRepositoryUpdater
 {
@@ -126,7 +68,11 @@ public static class ConfigRepositoryUpdater
 
         foreach (var entry in entries)
         {
-            if (!Repository.IsValid(entry.Path)) continue;
+
+            if (Directory.Exists(Path.Combine(entry.Path, ".git")) == false)
+            {
+                continue;
+            }
 
             var isOutdated = await repositoryClient.IsOutdated(entry.Path);
 
