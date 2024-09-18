bca0262e2329e25c4f9404ba2ecd210386c5818d
Cezary Piatek <cezarypiatek@live.com>
2024-09-18 19:27:00 +0200

    Add nuget source explicitly for updater

--- src/ScriptRunner/AppInstaller/Program.cs
+++ src/ScriptRunner/AppInstaller/Program.cs
@@ -42,7 +42,9 @@ internal class Program
         updateDotnetToolCommand.SetHandler((packageName, version) =>
         {
             Console.WriteLine($"Updating dotnet tool {packageName}");
-            var command = string.IsNullOrWhiteSpace(version) == false? $"tool update {packageName} --global --no-cache --ignore-failed-sources --version {version}": $"tool update {packageName} --global --no-cache --ignore-failed-sources";
+            var command = string.IsNullOrWhiteSpace(version) == false 
+                ? $"tool update {packageName} --global --no-cache --ignore-failed-sources --version {version} --add-source https://api.nuget.org/v3/index.json"
+                : $"tool update {packageName} --global --no-cache --ignore-failed-sources --add-source https://api.nuget.org/v3/index.json";
             var process = Process.Start("dotnet", command);
             process.WaitForExit();
             if (process.ExitCode != 0)
