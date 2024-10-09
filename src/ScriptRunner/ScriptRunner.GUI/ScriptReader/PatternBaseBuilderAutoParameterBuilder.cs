01f6d1cdc5f38572e9bdc7b12bb23f9de636e5a6
Cezary Piatek <cezarypiatek@live.com>
2024-10-09 20:49:32 +0200

    Add option to exclude parameter from AutoParameterBuilder

--- src/ScriptRunner/ScriptRunner.GUI/ScriptReader/PatternBaseBuilderAutoParameterBuilder.cs
+++ src/ScriptRunner/ScriptRunner.GUI/ScriptReader/PatternBaseBuilderAutoParameterBuilder.cs
@@ -4,15 +4,20 @@ namespace ScriptRunner.GUI.ScriptReader;
 
 class PatternBaseBuilderAutoParameterBuilder : IAutoParameterBuilder
 {
-    private readonly string _pattern;
+    private readonly string? _pattern;
 
-    public PatternBaseBuilderAutoParameterBuilder(string pattern)
+    public PatternBaseBuilderAutoParameterBuilder(string? pattern)
     {
         _pattern = pattern;
     }
 
     public string Build(ScriptParam param)
     {
+        if(param.SkipFromAutoParameterBuilder)
+        {
+            return string.Empty;
+        }
+
         var actionAutoParameterBuilderPattern = param.AutoParameterBuilderPattern ?? _pattern ?? string.Empty;
         return actionAutoParameterBuilderPattern
             .Replace("{name}", param.Name)
