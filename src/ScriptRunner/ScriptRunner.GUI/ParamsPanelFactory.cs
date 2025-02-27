33ab9f1a1c01cc53c64929b0e0547ec1efda632e
Cezary Piatek <cezarypiatek@live.com>
2025-02-27 22:14:39 +0100

    Set FileName for FileWithContent in a deterministic way based on the content

--- src/ScriptRunner/ScriptRunner.GUI/ParamsPanelFactory.cs
+++ src/ScriptRunner/ScriptRunner.GUI/ParamsPanelFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Avalonia;
@@ -520,19 +521,31 @@ public class PasswordControl : IControlRecord
 }
 public class FileContent : IControlRecord
 {
+    private readonly string _extension;
     public Control Control { get; set; }
     public string FileName { get; set; }
 
     public FileContent(string extension)
     {
+        _extension = extension;
         FileName = Path.GetTempFileName() + "." + extension;
     }
 
     public string GetFormattedValue()
     {
         var fileContent = ((TextBox)Control).Text;
+        var hash = string.IsNullOrWhiteSpace(fileContent)? "EMPTY" : ComputeSHA256(fileContent).Substring(0,10);
+        FileName = Path.Combine(Path.GetTempPath(), hash + "." + _extension);
         File.WriteAllText(FileName, fileContent, Encoding.UTF8);
         return FileName;
+    }
+    
+    static string ComputeSHA256(string input)
+    {
+        using var sha256 = SHA256.Create();
+        byte[] bytes = Encoding.UTF8.GetBytes(input);
+        byte[] hashBytes = sha256.ComputeHash(bytes);
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
     }
 
     public string Name { get; set; }
